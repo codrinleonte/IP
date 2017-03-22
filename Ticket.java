f0f608f4957e0354b53be5fcf807b8afad327556
codrinleonte <codryn_14@yahoo.com>
2017-03-22 12:42:02 +0200

    clasa Ticket

---
+++ Ticket.java
@@ -0,0 +1,29 @@
+import java.util.Vector;
+
+public class Ticket {
+
+  public Movie movie;
+
+  public Double price;
+
+  public Sala sala;
+
+    public Vector <Movie> myMovie;
+    public Vector <Sala> mySala;
+    public Vector <SystemManager> mySystemManager;
+
+  public void setMovie(Movie m) {
+  }
+
+  public Movie getMovie() {
+  return null;
+  }
+
+  public Double getPrice() {
+  return null;
+  }
+
+  public void setPrice(double p) {
+  }
+
+}
